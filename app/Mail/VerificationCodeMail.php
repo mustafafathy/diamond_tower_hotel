095001f0561edf05bf7f8cf16e3c34968488d778
mustafa <45427497+mustafafathy@users.noreply.github.com>
2024-12-28 23:37:16 +0200

    Update VerificationCodeMail.php

--- app/Mail/VerificationCodeMail.php
+++ app/Mail/VerificationCodeMail.php
@@ -29,34 +29,4 @@ class VerificationCodeMail extends Mailable
         return $this->view('emails.verification_code')
         ->with('verificationCode', $this->verificationCode);
     }
-
-    /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
-    {
-        return new Envelope(
-            subject: 'Verification Code Mail',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
-    }
 }
