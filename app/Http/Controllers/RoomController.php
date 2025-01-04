0923532bb14798a144388b7348ef3344181510ef
mustafa <45427497+mustafafathy@users.noreply.github.com>
2025-01-04 16:10:48 +0200

    Update RoomController.php

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -174,7 +174,7 @@ class RoomController extends Controller
             'promoCode' => $request->promoCode,
         ];
 
-        return ['count' => 1, [new RoomResource($room, $additionalParams)]];
+        return ['count' => 1, 'data' =>[new RoomResource($room, $additionalParams)]];
     }
 
 
