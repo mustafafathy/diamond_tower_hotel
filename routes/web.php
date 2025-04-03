ce02833ff2ce64b4eb756ec063d60fbd86bc73c5
mustafafathy <mostaafafathy11999@gmail.com>
2025-04-03 06:28:47 +0200

    add_needed_routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Artisan; 
 
 /*
 |--------------------------------------------------------------------------
@@ -26,4 +27,33 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+});
+
+Route::get('/git-pull', function () {
+    // Ensure the environment is correct (optional)
+    $output = null;
+    $resultCode = null;
+
+    // Execute the git pull command
+    exec('git pull', $output, $resultCode);
+
+    // Return the output to the browser
+    return response()->json([
+        'output' => $output,
+        'resultCode' => $resultCode,
+    ]);
+});
+
+Route::get('/migrate', function () {
+    // Running the migrate command
+    $output = null;
+    $resultCode = null;
+
+    exec('php artisan migrate', $output, $resultCode);
+
+    // Return the output and result code
+    return response()->json([
+        'output' => $output,
+        'resultCode' => $resultCode,
+    ]);
 });
